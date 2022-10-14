a2d81ea0f9216cd4115f960a2c1dbf1a7411463c
Emily Rowley <erowley49@gmail.com>
2022-10-14 13:49:05 -0600

    working

--- chamber/javascript/script.js
+++ chamber/javascript/script.js
@@ -1,3 +1,31 @@
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    const menu = document.querySelector('.navigation')[0].classList.toggle("responsive");
 }
+
+function getDate(){
+    const today = new Date();
+    var weekdays = new Array(7);
+        weekdays[0] = "Sunday";
+        weekdays[1] = "Monday";
+        weekdays[2] = "Tuesday";
+        weekdays[3] = "Wednesday";
+        weekdays[4] = "Thursday";
+        weekdays[5] = "Friday";
+        weekdays[6] = "Saturday";
+        var weekday = weekdays[today.getDay()];
+    const day = today.getDate()
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    
+    const month = months[today.getMonth()];
+    const year = today.getFullYear();
+
+    document.querySelector('.date').innerHTML = `${weekday}, ${day} ${month} ${year}`;
+}
+
+function main()
+{
+    getDate();
+    toggleMenu();
+}
+
+main();
