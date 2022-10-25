df48d31c8b5e475f80a23f5dff21eb139df878a5
Emily Rowley <erowley49@gmail.com>
2022-10-25 11:20:36 -0600

    chamber invite

--- chamber/javascript/script.js
+++ chamber/javascript/script.js
@@ -37,12 +37,27 @@ function lastModified(){
     document.querySelector("#lastmodification").innerHTML = "Last Modification " + modified;
 }
 
+function chamberInvite(){
+    const invite = document.createElement("div");
+    const node = document.createTextNode("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.");
+    invite.appendChild(node);
+    document.querySelector(".date").appendChild(invite); 
+
+}
+
 function main()
 {
     getDate();
     toggleMenu();
     copyright();
     lastModified();
+
+    const today = new Date().getDay();
+    if (today == 1 || today == 2){
+        chamberInvite();
+    }
+
+
 }
 
 main();
